68c23cde8e639cdd87c292b9c9b11f0f95d58596
TJ Wukitsch <thomaswukitsch@gmail.com>
2023-07-24 16:32:20 -0400

    exercises 6 and 7

---
+++ exercises/ex6.py
@@ -0,0 +1,25 @@
+# f-Strings and variable assignment
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+# This is the only one I dont really understand yet. It looks like we are defining or applying a function maybe?
+# Applying the format function? IDK here.
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# Concatenate the two strings.
+print (w + e)
